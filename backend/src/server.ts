ab0f5026a9f0d983d6173d971c1679b54510b1b1
Johnny-Saraiva <johnny.saraiva.ads@gmail.com>
2020-06-19 20:46:46 -0300

    Added typescript, ts-node-dev

---
+++ backend/src/server.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (request, response) => {
+  return response.json({ message: 'Hello Setup Template!!!'});
+});
+
+app.listen(3333, err => {
+  if (err) {
+    console.log('Internal server error!');
+  } else {
+    console.log('🚀 Server is running...');
+  }
+});
