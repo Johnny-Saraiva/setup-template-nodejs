2fe633b177b764d755b42d336d329adfaefcffcd
Johnny-Saraiva <johnny.saraiva.ads@gmail.com>
2020-06-19 21:50:07 -0300

    Configured eslint file

--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (request, response) => {
-  return response.json({ message: 'Hello Setup Template!!!'});
+  return response.json({ message: 'Hello Setup Template!!!' });
 });
 
 app.listen(3333, err => {
